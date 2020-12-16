96676e2f6d8c76bdd814e98d23489adfc6081ed1
WesDea <westbury.d.a@gmail.com>
2020-12-16 15:36:43 +1000

    fixed database connection issue

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ import handleProfile from './controllers/profile.js';
 
 const db = knex({
     client: 'pg',
+    connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  });
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+});
 
 
 
